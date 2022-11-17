f2591a0b1a463dbb246cec95577343b4c4375011
Andre Renaud <andre@ignavus.net>
2022-11-17 17:05:10 +1300

    Use correct device ID when opening HCI device
    This fixes an issue where there is only one HCI device available, but it's called hci1, not hci0.

--- linux/hci/socket/socket.go
+++ linux/hci/socket/socket.go
@@ -77,7 +77,7 @@ func NewSocket(id int) (*Socket, error) {
 	}
 	var msg string
 	for id := 0; id < int(req.devNum); id++ {
-		s, err := open(fd, id)
+		s, err := open(fd, int(req.devRequest[id].id))
 		if err == nil {
 			return s, nil
 		}
